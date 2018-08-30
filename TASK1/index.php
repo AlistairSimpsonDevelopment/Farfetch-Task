a90ac3ad96eb9caf6d5a384d182fd13c4b8222f4
AlistairSimpsonDevelopment <asimpson.roomio@gmail.com>
2018-08-30 19:00:09 +0100

    task 1

---
+++ TASK1/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  
+<!-- meta -->
+<title>Farfetch Dev Task</title>
+<link rel="alternate" hreflang="x-default" href="website name here" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="description" content="Description of the site / page" />
+<meta name="keywords" content="Keywords to optimise for " />
+<meta name="robots" content="index, follow" />
+
+<!-- css -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+<link rel="shortcut icon" href="/favicon.ico" />
+<link rel="stylesheet" href="index.css">
+<link rel="stylesheet" href="mobile.css">
+<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Playfair+Display" rel="stylesheet">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+
+
+<!-- jquery & bootstrap -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+
+
+</head>
+
+<body>
+
+<?php include 'Modules/navbar.html'; ?>
+
+<?php include 'Modules/POS.html'; ?>
+
+<?php include 'Modules/hero.html'; ?>
+
+
+</body>
+</html>
