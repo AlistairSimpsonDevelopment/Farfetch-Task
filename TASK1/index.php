695bf7f178d4770696d5ac47c04d15aeaacfcf86
AlistairSimpsonDevelopment <asimpson.roomio@gmail.com>
2018-08-31 16:18:06 +0100

    more clean up

--- TASK1/index.php
+++ TASK1/index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 <!-- meta -->
-<title>Farfetch Dev Task</title>
+<title>Farfetch Developer Task 1</title>
 <link rel="alternate" hreflang="x-default" href="website name here" />
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
