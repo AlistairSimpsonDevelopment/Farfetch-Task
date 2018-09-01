085a63fdc8e58a8825ddfebdc4056ae0c091f3a0
AlistairSimpsonDevelopment <asimpson.roomio@gmail.com>
2018-09-01 15:46:45 +0100

    task 2 improvements

--- TASK2/response.php
+++ TASK2/response.php
@@ -2,17 +2,32 @@
 
 //$postdata 	= file_get_contents("php://input");
 //$request 	= json_decode($postdata, true);
-$productid  = $_REQUEST['ProductID'];
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = test_input($_REQUEST['ProductID']);
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+	$productid  = $_REQUEST['ProductID'];
 
 	$url ="https://www.farfetch.com/uk/api/ecommerce/products/";
 	
 	$RequestURL = $url . $productid;		 
-	 
-	$source = json_decode(file_get_contents($RequestURL),true);
+	$data = "";
+	$json = @file_get_contents($RequestURL);
+	 if($json===false) {
+		 
+	 } else {
+		$data = json_decode(file_get_contents($RequestURL),true);
+	 }
 			
-	$data = $source['categories'];
- 
-	
+		
 	echo json_encode($data) ;
 	
 ?>
